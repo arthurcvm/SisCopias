4df5d8ac0cb7961170b3247781a74d5b055439b4
Arthur Cezar <arthurcvm@hotmail.com>
2021-08-09 17:19:23 -0300

    feat: crud of trabalhos

--- back/app/Controllers/Http/TrabalhosController.ts
+++ back/app/Controllers/Http/TrabalhosController.ts
@@ -1,24 +1,120 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { badRequest, success } from 'App/Helpers/http-helper'
+import validation from 'App/localization/validation'
+import Trabalho from 'App/Models/Trabalho'
+import { errorHandle } from 'App/utils/errors-messages'
+import { queryHandle } from 'App/utils/query-builder'
+import { successHandle } from 'App/utils/success-messages'
+import {
+  createSchema,
+  indexSchema,
+  showSchema,
+  updateSchema,
+} from 'App/Validators/TrabalhoValidator'
 
 export default class TrabalhosController {
-  public async index ({}: HttpContextContract) {
+  public async index({ response, request }: HttpContextContract): Promise<void> {
+    try {
+      const validatedData = await request.validate({
+        schema: indexSchema,
+        messages: validation,
+      })
+
+      const query = Trabalho.query()
+      const trabalho = await queryHandle(query, validatedData, request.qs().search)
+
+      success(response, successHandle(trabalho, 'Filtro realizado com sucesso'))
+    } catch (error) {
+      const fields = {
+        page: 'página',
+        per_page: 'por página',
+        order_column: 'coluna de ordenação',
+        order: 'ordem',
+        filter: 'filtro',
+      }
+      badRequest(response, errorHandle(error.messages.errors, fields))
+    }
   }
 
-  public async create ({}: HttpContextContract) {
+  public async store({ response, request }: HttpContextContract): Promise<void> {
+    try {
+      const validatedData = await request.validate({
+        schema: createSchema,
+        messages: validation,
+      })
+
+      const trabalho = await Trabalho.create(validatedData)
+
+      success(response, successHandle(trabalho, 'Trabalho criado com sucesso'))
+    } catch (error) {
+      const fields = {
+        num_originais: 'número de originais',
+        qtd_copias: 'quantidade de cópias',
+        frente_verso: 'frente e verso',
+        solicitacao_id: 'solicitação',
+      }
+      badRequest(response, errorHandle(error.messages.errors, fields))
+    }
   }
 
-  public async store ({}: HttpContextContract) {
+  public async show({ request, response }: HttpContextContract): Promise<void> {
+    try {
+      const validatedData = await request.validate({
+        schema: showSchema,
+        messages: validation,
+      })
+
+      const trabalho = await Trabalho.query().where('id', validatedData.params.id).firstOrFail()
+
+      success(response, successHandle(trabalho))
+    } catch (error) {
+      const fields = { 'params.id': 'id' }
+      badRequest(response, errorHandle(error.messages.errors, fields, 'id incorreto'))
+    }
   }
 
-  public async show ({}: HttpContextContract) {
+  public async update({ params, request, response }: HttpContextContract): Promise<void> {
+    const trabalho = await Trabalho.find(params.id)
+
+    if (trabalho) {
+      try {
+        const validatedData = await request.validate({
+          schema: updateSchema,
+          messages: validation,
+        })
+
+        trabalho.merge(validatedData)
+        await trabalho.save()
+
+        success(response, successHandle(trabalho, 'Trabalho atualizado com sucesso'))
+      } catch (error) {
+        const fields = {
+          num_originais: 'número de originais',
+          qtd_copias: 'quantidade de cópias',
+          frente_verso: 'frente e verso',
+          solicitacao_id: 'solicitação',
+        }
+        badRequest(response, errorHandle(error.messages.errors, fields))
+      }
+    }
   }
 
-  public async edit ({}: HttpContextContract) {
-  }
+  public async destroy({ request, response }: HttpContextContract): Promise<void> {
+    try {
+      const validatedData = await request.validate({
+        schema: showSchema,
+        messages: validation,
+      })
 
-  public async update ({}: HttpContextContract) {
-  }
+      const trabalho = await Trabalho.find(validatedData.params.id)
+      if (trabalho) {
+        await trabalho.delete()
 
-  public async destroy ({}: HttpContextContract) {
+        success(response, successHandle(trabalho, 'Trabalho excluído com sucesso'))
+      }
+    } catch (error) {
+      const fields = { 'params.id': 'id' }
+      badRequest(response, errorHandle(error.messages.errors, fields, 'id incorreto'))
+    }
   }
 }
