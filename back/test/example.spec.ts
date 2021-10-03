24acbe54351f3f4d670ed45acad1ae6a74ccc21d
Arthur Cezar <arthurcvm@hotmail.com>
2021-10-02 21:59:55 -0300

    chore: configure japa test runner

---
+++ back/test/example.spec.ts
@@ -0,0 +1,22 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Welcome', () => {
+  test('ensure home page works', async (assert) => {
+    /**
+     * Make request
+     */
+    const { text } = await supertest(BASE_URL).get('/').expect(200)
+
+    /**
+     * Construct JSDOM instance using the response HTML
+     */
+    const { document } = new JSDOM(text).window
+
+    const title = document.querySelector('.title')
+    assert.exists(title)
+    assert.equal(title!.textContent!.trim(), 'It Works!')
+  })
+})
